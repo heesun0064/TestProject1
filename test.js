7c42cdeda58cf1c2e8c9d530ad589bb4fc35d570
spiderman <spiderman@xhift.co.kr>
2018-03-15 14:52:04 +0900

    added console print

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log("hello?");
 console.log("Hello???");
 console.log("======== version 3");
 console.log("this is done in github webpage. version 2.");
+console.log("this is done in webstorm...");
